49eaa4bb1b3a87a9ecb22dc762be8a3ee5abf285
Cristian Aranguren <caranguren59@unisalle.edu.co>
2026-04-09 21:20:21 -0500

    Se realiza el ejercicio 1

--- Taller5/ejercicio1.py
+++ Taller5/ejercicio1.py
@@ -1,2 +1,5 @@
-numero = int(input("Escoge un numero: "))
-print("El doble del numero es: ", numero * 2)
+N = int(input("Escoge un numero impar entero: "))
+Impar = ((N + 1)//2)**2
+if N % 2 == 0:
+     raise ValueError("Ese no es un numero impar")
+print("La suma de todos los numeros impares entre 1 y", N, "es: ",Impar)
